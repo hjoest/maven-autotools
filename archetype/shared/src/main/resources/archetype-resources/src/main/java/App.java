d62b2ce13b0618e9f90eb3820079cd8eb96615ad
Holger Joest <holger@joest.org>
2011-03-05 20:36:51 +0100

    Improved loading of a native library in the archetype code
    The archetype for a shared library now provides some code to determine
    the system architecture and the operating system as well as the file
    extender of shared libraries.  All this is necessary to load a native
    library in the method 'loadNativeLibrary'.
    
    It should be noted that there's some duplication of code originally
    found in 4367d3e9d2.  Although this violates the DRY (don't repeat
    yourself) principle, we refrain from introducing an additional
    dependency in the archetype code.

--- archetype/shared/src/main/resources/archetype-resources/src/main/java/App.java
+++ archetype/shared/src/main/resources/archetype-resources/src/main/java/App.java
@@ -28,8 +28,41 @@ public class App
 
     private static void loadNativeLibrary() {
         File nativeLibrary =
-              new File("target/autotools/install/lib/i386/linux/lib${artifactId}.so");
+              new File("target/autotools/install/lib/"
+                        + getSystemArchitecture() + "/"
+                        + getOperatingSystem() + "/"
+                        + "lib${artifactId}" + getSharedExt());
         System.load(nativeLibrary.getAbsolutePath());
+    }
+
+    private static String getOperatingSystem() {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.startsWith("windows")) {
+            os = "windows";
+        }
+        return os.replaceAll("[ /]", "");
+    }
+
+    private static String getSystemArchitecture() {
+        String arch = System.getProperty("os.arch").toLowerCase();
+        if (arch.equals("i386")) {
+           arch = "x86";
+        } else if (arch.equals("amd64")) {
+           arch = "x86_64";
+        } else if (arch.equals("powerpc")) {
+           arch = "ppc";
+        }
+        return arch;
+    }
+
+    private static String getSharedExt() {
+        String os = getOperatingSystem();
+        if ("windows".equals(os)) {
+            return ".dll";
+        } else if ("macosx".equals(os)) {
+            return ".dylib";
+        }
+        return ".so";
     }
 
 }
